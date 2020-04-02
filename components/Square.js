fcf18f57a2cc3c2aad6601026dec8a2650ff0242
yamotech <nothings.2c9@gmail.com>
2020-04-02 16:54:53 +0900

    :rewind: click

--- components/Square.js
+++ components/Square.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, Button, View, Alert } from 'react-native';
 
 export default class Square extends Component {
+  _handleButtonPress = () => {
+    Alert.alert(
+      'Button pressed!',
+      'You did it!',
+    );
+  };
+
   render() {
     return (
       <View style={styles.square}>
-        <Text style={styles.text}>{this.props.value}</Text>
+        <Button title={`${this.props.value}`} style={styles.button} onPress={this._handleButtonPress}/>
       </View>
     );
   }
@@ -15,10 +22,12 @@ const styles = StyleSheet.create({
   square: {
     width: 100,
     height: 100,
-    borderColor: '#333',
+    borderColor: '#000',
     borderWidth: 1,
   },
-  text: {
+  button: {
+    width: 100,
+    height: 100,
     fontSize: 80,
     textAlign: 'center'
   }
