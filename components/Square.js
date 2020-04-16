89dbd2dc2971c06b2dfae940670da194f180059c
yamotech <nothings.2c9@gmail.com>
2020-04-16 16:52:36 +0900

    :fire: 消し忘れ

--- components/Square.js
+++ components/Square.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { StyleSheet, Button, View, Alert } from 'react-native';
+import React from 'react';
+import { StyleSheet, Button, View } from 'react-native';
 
 export default function Square(props) {
   return (
