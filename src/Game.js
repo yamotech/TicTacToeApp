2d4fd27f03626e73345885d83f54d44cc5fd718f
yamotech <nothings.2c9@gmail.com>
2020-05-07 16:43:28 +0900

    :sparkles: タイムトラベルの実装

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 import Board from './Board';
 import calculateWinner from './calculateWinner'
@@ -7,16 +7,18 @@ import calculateWinner from './calculateWinner'
 export default class Game extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       history: [{
         squares: Array(9).fill(null),
       }],
-      xIsNext: true
+      stepNumber: 0,
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) return;
@@ -25,14 +27,34 @@ export default class Game extends Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
-    })
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move:
+        'Go to game start';
+
+      return (
+        <View key={move}>
+          <Button title={desc} onPress={() => this.jumpTo(move)} />
+        </View>
+      )
+    })
 
     let status;
     if (winner) {
@@ -51,7 +73,7 @@ export default class Game extends Component {
         </View>
         <View style={styles.gameInfo}>
           <Text>{status}</Text>
-          <View></View>
+          <View>{moves}</View>
         </View>
       </View>
     );
